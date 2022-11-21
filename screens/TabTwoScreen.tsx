6cf9857ec7977241f9731cd237f7a58e8b02983b
Pedro1450 <118778968+Pedro1450@users.noreply.github.com>
2022-11-21 20:23:27 -0300

    Add files via upload

---
+++ screens/TabTwoScreen.tsx
@@ -0,0 +1,29 @@
+import { Image, StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./dog.jpeg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
